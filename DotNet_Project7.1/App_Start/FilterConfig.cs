175db808458a3a4061ce3ca36a82edb9a4548500
Vishakha <raizadavishakha2000@gmail.com>
2023-02-14 02:12:52 +0530

    Initial commit

---
+++ DotNet_Project7.1/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace DotNet_Project7._1
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ DotNet_Project7.1/Controllers/StudentController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DotNet_Project7._1.Controllers
+{
+    public class StudentController : Controller
+    {
+        // GET: Student
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult LoadPartial()
+        {
+
+            return PartialView("_MyPartialView");
+        }
+    }
+}
